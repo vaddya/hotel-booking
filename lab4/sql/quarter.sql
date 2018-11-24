be1f5fd37fac98d8279fe891cf15526795c51160
Vadim Dyachkov <vadik.dyachkov@gmail.com>
2018-11-24 20:56:45 +0300

    #2 implement sql queries & init report

--- lab4/sql/quarter.sql
+++ lab4/sql/quarter.sql
@@ -1,19 +1,28 @@
-WITH summary AS (SELECT city,
-                        quarter,
-                        SUM(rcount)                                                        AS qcount,
-                        ROW_NUMBER() OVER (PARTITION BY quarter ORDER BY SUM(rcount) DESC) AS row
-                 FROM (SELECT city.id                                AS id,
-                              city.name                              AS city,
-                              EXTRACT(QUARTER FROM reservation.from) AS quarter,
-                              COUNT(1)                               AS rcount
-                       FROM reservation
-                                JOIN room ON room.id = reservation.room_id
-                                JOIN room_type ON room_type.id = room.room_type_id
-                                JOIN hotel ON hotel.id = room_type.hotel_id
-                                JOIN city ON hotel.city_id = city.id
-                       GROUP BY city.id, quarter) AS t1
-                 GROUP BY quarter, city
-                 ORDER BY quarter, qcount DESC)
-SELECT quarter, city, qcount
-FROM summary s
-WHERE s.row = 1
+WITH summary AS (
+    WITH quarter_summary AS (
+        SELECT city.id                      AS city_id,
+               EXTRACT(QUARTER FROM "from") AS quarter,
+               COUNT(*)                     AS count
+        FROM reservation
+            JOIN room ON room.id = reservation.room_id
+            JOIN room_type ON room_type.id = room.room_type_id
+            JOIN hotel ON hotel.id = room_type.hotel_id
+            JOIN city ON hotel.city_id = city.id
+        GROUP BY city.id, quarter
+    )
+    SELECT ROW_NUMBER() OVER (PARTITION BY quarter ORDER BY SUM(count) DESC) AS row_num,
+           city_id                                                           AS city_id,
+           quarter                                                           AS quarter,
+           SUM(count)                                                        AS reservations
+    FROM quarter_summary
+    GROUP BY quarter, city_id
+)
+SELECT summary.quarter      AS quarter,
+       city.name            AS city_name,
+       country.name         AS country_name,
+       summary.reservations AS reservations
+FROM summary
+         JOIN city ON summary.city_id = city.id
+         JOIN country ON city.country_id = country.id
+WHERE summary.row_num < 2
+ORDER BY summary.quarter, summary.reservations DESC;
---
+++ lab4/sql/travellers.sql
@@ -0,0 +1,38 @@
+WITH summary AS (
+    WITH increase_summary AS (
+        SELECT ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY "from") AS row_num,
+               price                                                    AS price,
+               user_id                                                  AS user_id
+        FROM reservation
+        WHERE is_paid AND user_id IN (
+            SELECT user_id
+            FROM reservation
+            WHERE is_paid
+            GROUP BY user_id
+            HAVING COUNT(*) > 10
+        )
+    )
+    SELECT curr.user_id                                       AS user_id,
+           AVG((curr.price - prev.price) :: numeric) :: money AS avg_diff
+    FROM increase_summary curr
+         JOIN increase_summary prev
+         ON curr.user_id = prev.user_id AND curr.row_num = prev.row_num + 1
+    GROUP BY curr.user_id
+    ORDER BY avg_diff DESC
+    LIMIT 5
+)
+SELECT usr.id, usr.name, usr.email, usr.phone_number, reservations, total, avg_diff
+FROM summary
+     JOIN "user" AS usr ON user_id = usr.id
+     JOIN (
+         SELECT user_id    AS user_id,
+                COUNT(*)   AS reservations,
+                SUM(price) AS total
+         FROM reservation
+         GROUP BY user_id
+     ) AS total ON total.user_id = usr.id;
+
+SELECT *
+FROM reservation
+WHERE user_id = 578 AND is_paid
+ORDER BY "from";
